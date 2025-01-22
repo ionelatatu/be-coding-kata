bea816ac7a586687ed473385a09dda28fa0bcbe4
ionelatatu <ionela.tatu19@gmail.com>
2025-01-22 10:50:05 +0200

    Initialize Spring Boot project

---
+++ src/test/java/com/ionela/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionela.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
